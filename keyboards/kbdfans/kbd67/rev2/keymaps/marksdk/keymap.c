34904542f1b2f00f4afe42200a6b44483d35115e
Mark Hougaard Jensen <mark@marks.dk>
2020-02-20 23:06:07 +0100

    chore: Cleaning up

--- keyboards/kbdfans/kbd67/rev2/keymaps/marksdk/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/marksdk/keymap.c
@@ -1,95 +1,65 @@
 #include QMK_KEYBOARD_H
 
-enum layers {
-    _QWERTY,
-    _FUNC,
-    _NUM
-};
-
-/*
-TODO: Make all Mac keyboard functions work on the _FUNC layer (brightness, volume etc.)
-TODO: How do you use the Numpad layer?
-*/
+enum layers { _QWERTY, _FUNC, _NUM };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Keymap (Base Layer) Default Layer
      * ,----------------------------------------------------------------.
-     * |ENu|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|   BS  |Play/Pause |
+     * |Esc/_NUM|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0| + | ´| BS|    |
      * |----------------------------------------------------------------|
-     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  {|  }| Ent |Next Track|
+     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  Å|  ^| Ent |    |
      * |------------------------------------------------------.    |----|
-     * |CapsFn|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|  #|    |Volume Down|
+     * |FUNC_|  A|  S|  D|  F|  G|  H|  J|  K|  L|  Æ|  Ø|  *|     |    |
      * |----------------------------------------------------------------|
-     * |Shift| <|  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Up |Fn  |
+     * |Shift| <|  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  -|Shift |Up |RGB |
      * |----------------------------------------------------------------|
      * |Ctrl|Win |Alt |          Space        |Alt |Ctrl|  |Lef|Dow|Rig |
-     * `------------------------------------------------'  `------------'
+     * `----------------------------------------------------------------'
      */
-    [_QWERTY] = LAYOUT_all(
-        LT(_NUM,KC_ESC),KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, KC_BSPC, KC_BSPC, KC_MEDIA_PLAY_PAUSE,
-        KC_TAB,           KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,       KC_MEDIA_NEXT_TRACK,
-        MO(1), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_ENT,             KC__VOLDOWN,
-        KC_LSFT, KC_GRV,    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,         KC_UP,        MO(_FUNC),
-        KC_LCTL, KC_LALT, KC_LGUI,       KC_SPC,         KC_SPC,          KC_SPC,          KC_RGUI,KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN,      KC_RGHT),
+    [_QWERTY] = LAYOUT_all(LT(_NUM, KC_ESC), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_BSPC, KC_MEDIA_PLAY_PAUSE, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, _______, MO(_FUNC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, _______, KC_LSFT, KC_GRV, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, RGB_TOG, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_SPC, KC_RGUI, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
 
-    /* Keymap Fn Layer
+    /* Keymap Fn Layer 
      * ,----------------------------------------------------------------.
-     * | ` | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|       |Ins |
+     * | System Sleep | Brightness Down (Mac)| Brightness Up (Mac)| F3| F4| F5| F6| Prev Track| Next Track| Mute|Vol Down|Vol Up|  |  |  |
      * |----------------------------------------------------------------|
-     * |     |TOG|M+ |H+ |S+ |V+ |Sp+|   |Prt|SLk|Pau|   |   |     |Play/Pause|
+     * |     |  |   |   |   |   |  |   |  |  |  |   |   |     |  |
      * |------------------------------------------------------.    |----|
-     * |      |VLK|M- |H- |S- |V+ |Sp-|   |   |   |   |   |   |    |Prev Track |
+     * |      |  |   |   |   |   |  |   |   |   |   |   |   |    |  |
      * |----------------------------------------------------------------|
-     * |       |BL |BL-|BL+|BRTG|   |   |   |   |   |   |      |PUp|    |
+     * |       |   |  |  |  |   |   |   |   |   |   |      |    PUp|    |
      * |----------------------------------------------------------------|
-     * |Sleep|Reset|    |         C+A+D      |C+A+I|Menu|  |Hom|PDn|End |
-     * `------------------------------------------------'  `------------'
+     * |   |   |    |                  |  |   |  |Hom|PDn|End |
+     * `---------------------------------------------------------------'
      */
-    [_FUNC] = LAYOUT_all(
-         KC_GRV,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______,_______,KC_INS,
-         _______,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI,_______,KC_PSCR,KC_SLCK,KC_PAUS,_______,_______,_______,      KC_MEDIA_PLAY_PAUSE,
-         _______,     VLK_TOG, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD,_______,_______,_______,_______,_______,      _______,      KC_MEDIA_PREV_TRACK,
-         _______,KC_GRV,BL_TOGG, BL_DEC, BL_INC, BL_BRTG,_______,_______,_______,_______,_______,_______,_______,        KC_PGUP,_______,
-         KC_SLEP,RESET  ,_______,      LCA(KC_DEL),     LCA(KC_DEL),    LCA(KC_DEL),  LCA(KC_INS),KC_APP, _______,KC_HOME,KC_PGDN,KC_END),
+    [_FUNC] = LAYOUT_all(KC_SYSTEM_SLEEP, KC_BRMD, KC_BRMU, KC_F3, KC_F4, KC_F5, KC_F6, KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, KC__MUTE, KC__VOLDOWN, KC__VOLUP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END),
 
     /* Keymap Numpad Layer
      * ,----------------------------------------------------------------'
-     * |   |  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  /|   |       |    |
+     * |   |  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  /|   |       |  |
      * |----------------------------------------------------------------|
      * |     |   |   |   |   |   |   |  4|  5|  6|  *|   |   |     |    |
      * |------------------------------------------------------.    |----|
      * |      |   |   |   |   |   |   |  1|  2|  3|  -|   |   |    |    |
      * |----------------------------------------------------------------|
-     * |     |  |NLk|   |   |   |   |   |  0|   |   |  +|      |   |    |
+     * |     |  |  |   |   |   |   |   |  0|   |   |  +|      |   |    |
      * |----------------------------------------------------------------|
      * |    |    |    |                       |    |    |  |   |   |    |
      * `------------------------------------------------'  `------------'
      */
-    [_NUM] = LAYOUT_all(
-        _______, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_0, KC_KP_SLASH, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_ASTERISK, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_MINUS, _______, _______,             _______,
-        _______, _______, KC_NLCK, _______, _______, _______, _______, _______, KC_KP_0, _______, _______, KC_KP_PLUS, _______,   _______, _______,
-        _______, _______, _______,       _______,        _______,         _______,          _______, _______, XXXXXXX, _______, _______, _______),
-    };
+    [_NUM] = LAYOUT_all(_______, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_0, KC_KP_SLASH, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_ASTERISK, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_MINUS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_KP_0, _______, _______, KC_KP_PLUS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
+};
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    return true;
-}
+bool process_record_user(uint16_t keycode, keyrecord_t *record) { return true; }
 
-void matrix_init_user(void) {
+void matrix_init_user(void) {}
 
-}
-
-void matrix_scan_user(void) {
-
-}
+void matrix_scan_user(void) {}
 
 void led_set_user(uint8_t usb_led) {
     if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-      rgblight_enable_noeeprom();
+        rgblight_enable_noeeprom();
     } else {
-      rgblight_disable_noeeprom();
+        rgblight_disable_noeeprom();
     }
 }
